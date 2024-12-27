66feaacfc5e3d87ed4769e935a0d67bb24588f68
Architecture Jobs India <tejasdgurav@gmail.com>
2024-12-27 17:42:42 +0530

    updated directory structure

--- app.py
+++ app.py
@@ -373,5 +373,9 @@ def index():
 
     return render_template('index.html')
 
+@app.route('/')
+def home():
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
